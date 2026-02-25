cc3138e9546562ca939a49fa74d2e965ed0e8f6d
jluk7 <jluk7@gatech.edu>
2026-02-25 02:53:37 -0500

    building out webpage interface and json for answers

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, send_file, send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_file('Jeopardy_main.html')
+
+@app.route('/gameboard')
+def gameboard():
+    return send_file('Jeopardy_gameboard.html')
+
+@app.route('/final')
+def final():
+    return send_file('Jeopardy_final.html')
+
+@app.route('/style.css')
+def style():
+    return send_file('style.css', mimetype='text/css')
+
+@app.route('/images/<path:filename>')
+def images(filename):
+    return send_from_directory('images', filename)
+
+@app.route('/answers.json')
+def answers_json():
+    return send_file('answers.json', mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(debug=True)
